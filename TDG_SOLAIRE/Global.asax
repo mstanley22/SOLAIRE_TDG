ca3baa3ff8878075dd56b372f4cdb9022ca272dd
VANESSA-CAMPOS\Vanessa <v.campos.sv@outlook.com>
2017-07-29 23:23:59 -0600

    Add project files.

---
+++ TDG_SOLAIRE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TDG_SOLAIRE.MvcApplication" Language="C#" %>
---
+++ TDG_SOLAIRE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDG_SOLAIRE.Startup))]
+namespace TDG_SOLAIRE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
